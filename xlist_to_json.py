50bceaaa098b22d6c1c7d06002d113e63c93578a
takahr2 <ryuga.takahashi@dell.com>
2019-12-11 00:42:54 +0900

    update

--- xlist_to_json.py
+++ xlist_to_json.py
@@ -7,15 +7,63 @@ class xinfo_to_clusterjson:
         self.xinfodict = xinfodict
     
     def create_dict_forexcel(self):
-        xmsdict = {}
+        
+        xmsdict = {}       
+        
+        for xmsinfo in self.xinfodict['xms']:
+            xmsdict[xmsinfo[0] ]= {
+                'name': xmsinfo[0],
+                'SW-Version': xmsinfo[2],
+                'Xms-IP-Addr-With-Subnet': xmsinfo[3]}
+
         for clusterinfo in self.xinfodict['clusters']:
-            xmsdict[clusterinfo[0]] = []
+            xmsdict[clusterinfo[0]] = [{
+                'clusterinfo': [],
+                'volumeinfo': [],
+                'lunmappings': [],
+                'snapsets': [],
+                'cg': [],
+                'targets': [],
+                'initiators': []
+                }]
+        
+        for clusterinfo in self.xinfodict['clusters']:
+            clusterinfosub = {}
+            clusterinfosub['cluster-Name'] = clusterinfo[0]
+            clusterinfosub['index'] = clusterinfo[1]
+            clusterinfosub['SW-Version'] = clusterinfo[14]
+            clusterinfosub['SerialNumber'] = clusterinfo[15]
+
+            xmsdict[clusterinfo[0]][0]['clusterinfo'].append({'clusterinfo': clusterinfosub})
+        
+        for volumeinfo in self.xinfodict['volumes']:
+            volumeinfosub = {}
+            volumeinfosub['Volume-Name'] = volumeinfo[0]
+            volumeinfosub['index'] = volumeinfo[1]
+            volumeinfosub['cluster-Name'] = volumeinfo[2]
+            volumeinfosub['Vol-Size'] = volumeinfo[4]
+            volumeinfosub['NAA-Identifier'] = volumeinfo[14]
+
+            xmsdict[volumeinfo[2]][0]['volumeinfo'].append(volumeinfosub)
+
+        for lunmappingsinfo in self.xinfodict['lunmappings']:
+            mappingssub = {}
+            mappingssub['Cluster-Name'] = lunmappingsinfo[0]
+            mappingssub['index'] = lunmappingsinfo[3]
+            mappingssub['Volume-Name'] = lunmappingsinfo[2]
+            mappingssub['IG-Name'] = lunmappingsinfo[4]
+            mappingssub['HLU'] = lunmappingsinfo[8]
+
+            xmsdict[lunmappingsinfo[0]][0]['lunmappings'].append(mappingssub)
+
         return xmsdict
         
 if __name__ == '__main__':
     testdict = read_outfiles.concatinate_to_dict('xmcli')
     forexldict = xinfo_to_clusterjson(testdict).create_dict_forexcel()
-    print(forexldict)
+    pprint.pprint(forexldict['vxi08-AC'][0]['lunmappings'])
+    #pprint.pprint(forexldict['xms'])
+    #pprint.pprint(forexldict)
 
 
 
