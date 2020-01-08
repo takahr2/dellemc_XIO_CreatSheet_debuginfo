71c2906e5deea7c7827f06443233631faa888a32
takahr2 <ryuga.takahashi@dell.com>
2020-01-08 13:04:01 +0900

    update 20200108_2

--- debuginfo_to_dws.py
+++ debuginfo_to_dws.py
@@ -2,12 +2,14 @@ import pprint
 import libs.debuginfo_extract as extract
 import libs.read_outfiles as outfiles
 import libs.xlist_to_json as xjson
+import libs.xjson_to_dws as todws
 import os
 import pathlib
 
 def debuginfo_to_json():
-    inputdirlist= pathlib.Path('input').iterdir()
+    inputdirlist = pathlib.Path('input').iterdir()
     fileidx = 1
+    xjsonlist = []
     for difile in inputdirlist:
         if '.tar' in difile.name:
             xmclidir = extract.debuginfo_extract(difile).xmcli_extract()
@@ -21,9 +23,19 @@ def debuginfo_to_json():
             jsonfilename = str(fileidx) + '_xio.json'
             jsonfile = xjson.json_dump(xiojson, jsonfilename)
             fileidx += 1
+            xjsonpath = os.path.join('input', jsonfilename)
+            print(xjsonpath)
+            xjsonfullpath = os.path.abspath(xjsonpath)
 
+            xjsonlist.append(xjsonfullpath)
+        
+    return xjsonlist
+
+def main():
+    clidir = debuginfo_to_json()
+    print(clidir)
 
 if __name__ == '__main__':
-    debuginfo_to_json()
+    main()
     
 
